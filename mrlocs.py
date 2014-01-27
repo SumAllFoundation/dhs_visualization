43fbb7385e30c1186776becbc2c15b488bac24b6
Abhi Ramachandran <abhi@sumall.org>
2014-01-27 10:41:56 -0500

    safe version of mrlocs.py

--- mrlocs.py
+++ mrlocs.py
@@ -7,16 +7,16 @@ class MRLocateBorough(MRJob):
 		with open('boroughs.geojson') as f: boroughs = json.loads(f.read())
 		#with open('blocks.geojson') as f: blocks = json.loads(f.read())
 		#with open('tracts.geojson') as f: tracts = json.loads(f.read())
-		with open('neighborhoods.geojson') as f: neighborhoods = json.loads(f.read())
+		with open('neighborhoods.geojson') as f: nhoods = json.loads(f.read())
 		with open('sdistricts.geojson') as f: sdistricts = json.loads(f.read())
 		with open('pprecincts.geojson') as f: pprecincts = json.loads(f.read())
 		with open('zctas.geojson') as f: zctas = json.loads(f.read())
 
 		#Extract the coordinates. feature keys: [u'type', u'coordinates']
-		self.borough_polygons = [(shape(feature['geometry']), feature['properties']['borough']) for feature in boroughs['features'] ]
-		#self.block_polygons =  [(shape(feature['geometry']), feature['properties'][] for feature in blocks['features'] ]
-		#self.tract_polygons =  [shape(feature['geometry']) for feature in tracts['features'] ]
-		self.neighborhood_polygons =  [(shape(feature['geometry']), feature['properties']['neighborhood']) for feature in neighborhoods['features'] ]
+		self.borough_polygons = [(shape(feature['geometry']), feature['properties']['borough']) for feature in boroughs['features']]
+		#self.block_polygons =  [(shape(feature['geometry']), feature['properties'][] for feature in blocks['features']]
+		#self.tract_polygons =  [shape(feature['geometry']) for feature in tracts['features']]
+		self.nhood_polygons =  [(shape(feature['geometry']), feature['properties']['neighborhood']) for feature in nhoods['features'] ]
 		self.sdistrict_polygons =  [(shape(feature['geometry']), feature['properties']['schoolDistrict']) for feature in sdistricts['features'] ]
 		self.pprecinct_polygons =  [(shape(feature['geometry']), feature['properties']['policePrecinct']) for feature in pprecincts['features'] ]	
 		self.zcta_polygons =  [(shape(feature['geometry']), feature['properties']['postalCode']) for feature in zctas['features'] ]	
@@ -24,17 +24,30 @@ class MRLocateBorough(MRJob):
 	def mapper(self, key, line):		
 		point= line.split(',')[4:6]
 	        point = Point(float(point[1]), float(point[0]))	
-	
-		temp_borough		= (borough[1] for borough in self.borough_polygons if borough[0].contains(point)).next()
+		try :	
+			temp_borough = (borough[1] for borough in self.borough_polygons if borough[0].contains(point)).next()
+		except:
+			temp_borough = "NA"
 		#temp_block   		= (block(1) for block in self.block_polygons) if block[0].contains(point)).next()
 		#temp_tract   		= (tract(1) for tract in (self.tract_polygons) if tract[0].contains(point)).next()
-		temp_neighborhood 	= (neighborhood[1] for neighborhood in self.neighborhood_polygons if neighborhood[0].contains(point)).next()
-		temp_sdistrict 		= (district[1] for district in self.sdistrict_polygons if district[0].contains(point)).next()
-		temp_pprecinct 		= (precinct[1] for precinct in	self.pprecinct_polygons if precinct[0].contains(point)).next()	
-		temp_zcta 		= (zcta[1] for zcta in self.zcta_polygons if zcta[0].contains(point)).next()	
-	
+		try:
+			temp_nhood = (nhood[1] for nhood in self.nhood_polygons if nhood[0].contains(point)).next()
+		except:
+			temp_nhood = "NA"
+		try:
+			temp_sdistrict = (district[1] for district in self.sdistrict_polygons if district[0].contains(point)).next()
+		except:
+			temp_sdistrict = "NA"
+		try:
+			temp_pprecinct = (precinct[1] for precinct in	self.pprecinct_polygons if precinct[0].contains(point)).next()	
+		except:
+			temp_pprecinct = "NA"
+		try:
+			temp_zcta = (zcta[1] for zcta in self.zcta_polygons if zcta[0].contains(point)).next()	
+		except:
+			temp_zcta = "NA"
 #		print temp_borough,temp_neighborhood, temp_sdistrict, temp_pprecinct, temp_zcta	
-		line = [line, temp_borough, temp_neighborhood, temp_sdistrict, temp_pprecinct, temp_zcta]
+		line = [line, temp_borough, temp_nhood, temp_sdistrict, temp_pprecinct, temp_zcta]
 		#line = ','.join(map(str, line))		
 		yield key,line
 
